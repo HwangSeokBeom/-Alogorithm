bc7491e3346757b18658ccee942e947c8233420b
HwangSeokBeom <49748207+HwangSeokBeom@users.noreply.github.com>
2024-11-08 19:01:40 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.swift
@@ -0,0 +1,17 @@
+func solution(_ num:Int) -> String {
+    var n = abs(num)
+    var s = "Even"
+    
+    if n % 2 == 0 {
+        s = "Even"
+    }
+    else if n % 2 == 1 {
+        s = "Odd"
+    }
+ 
+    
+    // switch num % 2
+    //     case 
+    
+    return s
+}
