b1147f91fea77b32ac50faaa74babb6388fc3945
HwangSeokBeom <49748207+HwangSeokBeom@users.noreply.github.com>
2024-11-21 20:00:24 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.swift
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -1,17 +1,15 @@
 import Foundation
 
 func solution(_ number:[Int]) -> Int {
-    
-    var result = 0
-    for i in 0..<number.count{
-        for j in i+1..<number.count{
-            for k in j+1..<number.count{
-                if number[i]+number[j]+number[k] == 0{
-                    result += 1    
-                }
+    var ans = 0
+
+    for i in 0..<number.count {
+        for j in i+1..<number.count {
+            for z in j+1..<number.count {
+                if number[i] + number[j] + number[z] == 0 { ans += 1 }
             }
         }
     }
-    
-    return result
+
+    return ans
 }
