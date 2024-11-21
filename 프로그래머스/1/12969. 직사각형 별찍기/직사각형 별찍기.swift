26545c3ecd84bf5c53e5bf77bf73f66464432a7f
HwangSeokBeom <49748207+HwangSeokBeom@users.noreply.github.com>
2024-11-21 09:32:43 +0900

    [level 1] Title: 직사각형 별찍기, Time: 46.75 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -4,8 +4,7 @@ let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
 var (a, b) = (n[0], n[1])
 
 for _ in 0..<b{
-    print("*", terminator: "")
-    for _ in 0..<a-1{
+    for _ in 0..<a{
         print("*", terminator: "")
     }
     print("")
