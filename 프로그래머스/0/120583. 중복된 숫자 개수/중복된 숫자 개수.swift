04bf795b34e6b4f2ebf6bcddceb32373a4ea20b0
HwangSeokBeom <49748207+HwangSeokBeom@users.noreply.github.com>
2024-11-25 09:35:35 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ array:[Int], _ n:Int) -> Int {
+    
+    var answer = 0
+    
+    for i in array{
+        if i == n{
+            answer += 1
+        }
+    }
+    
+    return answer
+}
